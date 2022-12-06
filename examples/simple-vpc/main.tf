52d3c96833d630b5d80a4d9a1f470239a38edbba
amir-projects <humairabinte.amir@gmail.com>
2022-12-06 22:46:03 +0600

    custom vpc config

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -3,42 +3,25 @@ provider "aws" {
 }
 
 locals {
-  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
+  #name   = "ex-${replace(basename(path.cwd), "_", "-")}"
   region = "ap-northeast-1"
-
-  tags = {
-    Name = local.name
-    #GithubRepo = "terraform-aws-vpc"
-    #GithubOrg  = "terraform-aws-modules"
-  }
 }
 
-################################################################################
-# VPC Module
-################################################################################
-
 module "vpc" {
-  source = "../../"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = local.name
+  name = "my-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  #enable_ipv6 = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-  enable_nat_gateway = false
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
-
-  tags = local.tags
-
-  vpc_tags = {
-    Name = "vpc-name"
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
